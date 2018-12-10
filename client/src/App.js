3c67bc39ac25b6e172bffaa849d41bc92e7bd0b4
riana randoja <rianarandoja@gmail.com>
2018-12-10 22:22:31 +0200

    Basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+
 import './App.css'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <h1>Dev-Net</h1>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          </div>
+          <Footer />
       </div>
+      </Router>
     )
   }
 }
