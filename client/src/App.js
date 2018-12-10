e787fe7a3610383b14c2aee9f7f3a75a552c4a71
riana randoja <rianarandoja@gmail.com>
2018-12-10 22:24:17 +0200

    Register

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,24 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
+        <div className="App">
           <Navbar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </div>
           <Footer />
-      </div>
+        </div>
       </Router>
     )
   }
